93731beaedb0cc14903492ff34c46e4d15dc254c
Viacheslav-ua <atph@ukr.net>
2025-04-15 14:41:53 +0000

    added dependences and make_offer feature

--- programs/escrowed/src/lib.rs
+++ programs/escrowed/src/lib.rs
@@ -15,7 +15,13 @@ declare_id!("FkZiBUzj8GngL9U61oaNK292864ennirVXVjejNVjuvP");
 pub mod escrowed {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
     }
 }
